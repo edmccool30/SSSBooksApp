a6b11eacfd68ea311dcf2fbfd447352973958084
edmccool30 <danieldalli5000@gmail.com>
2024-12-29 11:16:36 +0000

    Setup the Model as well as its relationships

--- bookapp/app/Models/Author.php
+++ bookapp/app/Models/Author.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'birthdate', 'nationality'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
